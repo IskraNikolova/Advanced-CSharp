05b8575d2d8035b11d8c7bcaf820f6fef17a7173
Iskra Nikolova <air_iskra@abv.bg>
2015-08-17 15:48:42 +0300

    Add a new problem

---
+++ Homework1-ArraysListsStacksQueues/Problem2-SortArrayOfNumbersUsingSelectionSort/SortArrayOfNumbersUsingSelectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+public class SortArrayOfNumbersUsingSelectionSort
+{
+    /// <summary>
+    /// A program to sort an array of numbers and then print them back on the console. 
+    /// </summary>
+    public static void Main()
+    {
+        int[] collectionOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        while (true)
+        {
+            bool hasSwapped = false;
+            for (int number = 0; number < collectionOfNumbers.Length - 1; number++)
+            {
+                if (collectionOfNumbers[number] > collectionOfNumbers[number + 1])
+                {
+                    int oldValue = collectionOfNumbers[number];
+                    collectionOfNumbers[number] = collectionOfNumbers[number + 1];
+                    collectionOfNumbers[number + 1] = oldValue;
+                    hasSwapped = true;
+                }
+             
+            }
+            if (hasSwapped == false)
+            {
+                break;
+            }
+        }
+        Console.WriteLine(string.Join(" ", collectionOfNumbers));
+    }
+}
+
