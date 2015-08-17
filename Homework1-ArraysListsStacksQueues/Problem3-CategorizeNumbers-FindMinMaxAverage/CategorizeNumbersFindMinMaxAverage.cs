649aa9fff4c410a048dcc52130b50cf4ebf58429
Iskra Nikolova <air_iskra@abv.bg>
2015-08-17 16:52:31 +0300

    Add a new problem

---
+++ Homework1-ArraysListsStacksQueues/Problem3-CategorizeNumbers-FindMinMaxAverage/CategorizeNumbersFindMinMaxAverage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CategorizeNumbersFindMinMaxAverage
+{
+    public static void Main()
+    {
+        double[] collectionOfNumbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+        List<double> roundNumbers = new List<double>();
+        List<double> notRoundNumbers = new List<double>();
+
+        for (int i = 0; i < collectionOfNumbers.Length; i++)
+        {
+            if (collectionOfNumbers[i] == (int) collectionOfNumbers[i])
+            {
+                roundNumbers.Add(collectionOfNumbers[i]);
+            }
+            else
+            {
+                notRoundNumbers.Add(collectionOfNumbers[i]);
+            }
+        }
+
+        Console.WriteLine("[" + string.Join(", ", notRoundNumbers) + "]"
+                              + "-> min: {0}, max: {1}, sum: {2}, avg: {3:F2}", 
+                              notRoundNumbers.Min(), notRoundNumbers.Max(), 
+                              notRoundNumbers.Sum(), notRoundNumbers.Average());
+
+        Console.WriteLine("[" + string.Join(", ", roundNumbers) + "]"
+                      + "-> min: {0}, max: {1}, sum: {2}, avg: {3:F2}",
+                      roundNumbers.Min(), roundNumbers.Max(),
+                      roundNumbers.Sum(), roundNumbers.Average());
+    }
+}
+
