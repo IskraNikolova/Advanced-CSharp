27bbc4e1d84fcd9d057e140d513d8ca111576162
Iskra Nikolova <air_iskra@abv.bg>
2015-08-21 16:10:43 +0300

    Add a Problem7- SortedSubsetSum

---
+++ Homework1-ArraysListsStacksQueues/Problem7-SortedSubsetSums/SortedSubsetSum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SortedSubsetSums 
+{
+    
+    static bool solution;
+    public static void Main()
+    {
+        Console.Write("sum: ");
+        int sum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Sequence of numbers: ");
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).Distinct().ToArray();
+        Array.Sort(numbers);
+
+        List<List<int>> subsets = new List<List<int>>();
+        List<int> subset = new List<int>();
+
+        MakeSubset(0, sum, subset, numbers, subsets);
+
+        var sorted = subsets.OrderBy(x => x.Count);
+        foreach (var item in sorted)
+        {
+            Console.WriteLine(" {0} = {1}", string.Join(" + ", item), sum);
+        }
+
+        if (!solution)
+        {
+            Console.WriteLine("No matching subsets.");
+        }
+      
+    }
+
+    public static void MakeSubset(int index, int sum, List<int> subset, int[] numbers, List<List<int>> subsets )
+    {
+        if (subset.Sum() == sum && subset.Count > 0) 
+        {
+            subsets.Add(new List<int>(subset));
+            solution = true; 
+        }
+
+        for (int i = index; i < numbers.Length; i++)
+        {
+            subset.Add(numbers[i]);
+            MakeSubset(i + 1, sum, subset, numbers, subsets); 
+            subset.RemoveAt(subset.Count - 1); 
+        }
+    }
+}
