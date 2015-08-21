d024951a66a08dde0371126aaba707d35b3abd3b
Iskra Nikolova <air_iskra@abv.bg>
2015-08-21 23:43:13 +0300

    Add a new problem

---
+++ Homework1-ArraysListsStacksQueues/Problem10-PythagoreanNumbers/PythagoreanNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class PythagoreanNumbers
+{
+    public static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+
+        int[] inputNumbers = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            inputNumbers[i] = number;
+        }
+
+        bool check = false;
+        for (int a = 0; a < inputNumbers.Length; a++)
+        {
+            for (int b = a + 1; b < inputNumbers.Length; b++)
+            {
+                for (int c = b + 1; c < inputNumbers.Length; c++)
+                {
+                    if (Math.Pow(inputNumbers[a], 2) + Math.Pow(inputNumbers[b], 2) == Math.Pow(inputNumbers[c], 2))
+                    {
+                        Console.WriteLine("{0}*{0} + {1}*{1} = {2}*{2}", inputNumbers[a],
+                                                        inputNumbers[b], inputNumbers[c]);
+
+                        check = true;
+                    }
+                }
+            }
+        }
+
+        if (!check)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
+
