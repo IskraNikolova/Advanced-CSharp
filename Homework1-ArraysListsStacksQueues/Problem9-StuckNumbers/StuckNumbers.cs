8788e66c4367e8535d2d6a634a7772fdcef5b82b
Iskra Nikolova <air_iskra@abv.bg>
2015-08-21 22:59:16 +0300

    Add a new problem - Stuck Numbers

---
+++ Homework1-ArraysListsStacksQueues/Problem9-StuckNumbers/StuckNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class StuckNumbers
+{
+    public static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        string[] inputNumbers = Console.ReadLine()
+            .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        bool check = false;
+        for (int i = 0; i < inputNumbers.Length; i++)
+        {
+            for (int j = 0; j < inputNumbers.Length; j++)
+            {
+                for (int k = 0; k < inputNumbers.Length; k++)
+                {
+                    for (int l = 0; l < inputNumbers.Length; l++)
+                    {
+                        if (inputNumbers[i] != inputNumbers[j] && inputNumbers[i] != inputNumbers[k] &&
+                            inputNumbers[i] != inputNumbers[l] && inputNumbers[j] != inputNumbers[k] &&
+                            inputNumbers[j] != inputNumbers[l] && inputNumbers[k] != inputNumbers[l])
+                        {
+                            if (inputNumbers[i] + inputNumbers[j] == inputNumbers[k] + inputNumbers[l])
+                            {
+                                Console.WriteLine("{0}|{1}=={2}|{3}", inputNumbers[i], inputNumbers[j],
+                                    inputNumbers[k], inputNumbers[l]);
+
+                                check = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (!check)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
