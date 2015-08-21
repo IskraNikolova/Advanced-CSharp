219057260cc99f6e48a3aa78da9886bb3f3411b0
Iskra Nikolova <air_iskra@abv.bg>
2015-08-21 21:05:08 +0300

    Add a problem8- LegoBlocks

---
+++ Homework1-ArraysListsStacksQueues/Problem8-LegoBlocks/LegoBlocks.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LegoBlocks
+{
+    public static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+
+        int[][] firstJaggedAray = new int[rows][];
+        int[][] secondJaggedAray = new int[rows][];
+
+
+        List<int> rowsOfFirstAray = new List<int>();
+        List<int> rowsOfSecondAray = new List<int>();
+        for (int i = 0; i < rows; i++)
+        {
+            rowsOfFirstAray = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            firstJaggedAray[i] = rowsOfFirstAray.ToArray();
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            rowsOfSecondAray = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            secondJaggedAray[i] = rowsOfSecondAray.ToArray();
+        }
+
+        bool check = true;
+
+
+        int legoRowLenght = firstJaggedAray[0].Length + secondJaggedAray[0].Length;
+        int counterAllBlocks = legoRowLenght;
+
+        for (int i = 1; i < rows; i++)
+        {
+            int leftSide = firstJaggedAray[i].Length;
+            int rightSide = secondJaggedAray[i].Length;
+            counterAllBlocks += leftSide + rightSide;
+            if (firstJaggedAray[i].Length + secondJaggedAray[i].Length != legoRowLenght)
+            {
+                check = false;
+            }
+        }
+
+        if (check)
+        {
+            int count = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                int length = firstJaggedAray[i].Length + secondJaggedAray[i].Length;
+
+
+                int[] result = new int[length];
+
+                for (int j = 0; j < length; j++)
+                {
+                    if (j < firstJaggedAray[i].Length)
+                    {
+                        result[j] = firstJaggedAray[i][j];
+                    }
+
+                    else
+                    {
+
+                        result[j] = secondJaggedAray[i][(secondJaggedAray[i].Length - 1) - count];
+                        count++;
+                    }
+                }
+
+                Console.WriteLine("[" + string.Join(", ", result) + "]");
+                count = 0;
+            }
+        }
+        else
+        {
+            Console.WriteLine("The total number of cells is: " + counterAllBlocks);
+        }
+    }
+}
