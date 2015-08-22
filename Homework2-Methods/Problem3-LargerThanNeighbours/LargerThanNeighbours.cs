bbb36beeaf2a06d65bfd3982cc52c8c6d1d72b52
Iskra Nikolova <air_iskra@abv.bg>
2015-08-22 20:29:42 +0300

    Add a new problem

---
+++ Homework2-Methods/Problem3-LargerThanNeighbours/LargerThanNeighbours.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+public class LargerThanNeighbours
+{
+    public static void Main()
+    {
+        int[] numberForCheck = Console.ReadLine()
+            .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int i = 0; i < numberForCheck.Length; i++)
+        {
+            Console.WriteLine(IsLargerThanNeighbours(numberForCheck, i));
+        }
+    }
+
+    /// <summary>
+    /// a method that checks if the element at given position in a given array of integers is larger 
+    /// than its two neighbours (when such exist).
+    /// </summary>
+    /// <param name="number">check number</param>
+    /// <param name="index">index of number in array</param>
+    /// <returns></returns>
+    public static bool IsLargerThanNeighbours(int[] number, int index)
+    {
+        bool isLarge = false;
+
+        if (index > 0 && index < number.Length - 1)
+        {
+            if (number[index] > number[index + 1] && number[index] > number[index - 1])
+            {
+               isLarge = true;
+            }
+        }
+        else
+        {
+            if (index == 0)
+            {
+                if (number[index] > number[index + 1])
+                {
+                    isLarge = true;
+                }
+            }
+            else if (index == number.Length)
+            {
+                if (number[index] > number[index - 1])
+                {
+                    isLarge = true;
+                }
+            }
+        }
+
+        return isLarge;
+    }
+}
+
