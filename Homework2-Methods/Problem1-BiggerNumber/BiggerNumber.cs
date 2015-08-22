95b522211dd7e1232b1dcf2a6cb865a22737883a
Iskra Nikolova <air_iskra@abv.bg>
2015-08-22 18:10:11 +0300

    Add a new homework

---
+++ Homework2-Methods/Problem1-BiggerNumber/BiggerNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class BiggerNumber
+{
+    public static void Main()
+    {
+        int firstInteger = int.Parse(Console.ReadLine());
+        int secondInteger = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(GetMax(firstInteger, secondInteger));
+    }
+
+    /// <summary>
+    /// A method GetMax() with two parameters that returns the larger of two integers. 
+    /// </summary>
+    /// <param name="firstInteger">first integer</param>
+    /// <param name="secondInteger">second integer</param>
+    /// <returns>the larger of two integers</returns>
+    public static int GetMax(int firstInteger, int secondInteger)
+    {
+        int biggerNumber;
+
+        if (firstInteger > secondInteger)
+        {
+            biggerNumber = firstInteger;
+        }
+        else
+        {
+            biggerNumber = secondInteger;
+        }
+        return biggerNumber;
+    }
+}
+
