68757b312b9aae62db393be52ae042318fc07f1c
Iskra Nikolova <air_iskra@abv.bg>
2015-08-23 08:28:04 +0300

    Add a new problem

---
+++ Homework2-Methods/Problem5-ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+public class ReverseNumber
+{
+    public static void Main()
+    {
+        double number = double.Parse(Console.ReadLine());
+        double reversed = GetReversedNumber(number);
+
+        Console.WriteLine(reversed);
+    }
+    /// <summary>
+    /// A method that reverses the digits of a given floating-point number.
+    /// </summary>
+    /// <param name="number">number to reverse</param>
+    /// <returns>reversed number</returns>
+    public static double GetReversedNumber(double number)
+    {
+        double reversedNumber = number;
+        string reverse = reversedNumber.ToString();
+
+        StringBuilder builderReverse = new StringBuilder();
+        for (int i = reverse.Length - 1; i >= 0; i--)
+        {
+            builderReverse.Append(reverse[i]);
+        }
+
+        reversedNumber = double.Parse(builderReverse.ToString());
+
+        return reversedNumber;
+    }
+}
+
