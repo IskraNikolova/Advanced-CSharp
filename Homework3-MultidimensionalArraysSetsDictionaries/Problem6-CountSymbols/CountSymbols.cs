d550b74c1e4eb136df556eef45aeb857cea575ce
IskraNikolova <air_iskra@yahoo.com>
2015-08-28 21:13:36 +0300

    Add a new problem

---
+++ Homework3-MultidimensionalArraysSetsDictionaries/Problem6-CountSymbols/CountSymbols.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CountSymbols
+{
+    /// <summary>
+    /// A program that reads some text from the console and counts the occurrences of each character in it.
+    ///  Print the results in alphabetical (lexicographical) order
+    /// </summary>
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        SortedDictionary<char, int> sortInputChars = new SortedDictionary<char, int>();
+
+        foreach (var element in input)
+        {
+            if (sortInputChars.ContainsKey(element))
+            {
+                sortInputChars[element]++;
+            }
+            else
+            {
+                sortInputChars.Add(element, 1);
+            }
+        }
+        foreach (var element in sortInputChars)
+        {
+            Console.WriteLine("{0}: {1} time/s", element.Key, element.Value);
+        }
+        Console.WriteLine(sortInputChars.Count());
+       
+
+    }
+}
+
