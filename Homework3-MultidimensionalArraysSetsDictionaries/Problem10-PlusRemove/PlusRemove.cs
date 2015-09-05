a3b71065e66e94aed9884f51e3d970fe8b82faf8
IskraNikolova <air_iskra@yahoo.com>
2015-09-05 23:07:42 +0300

    Add a cs fail

---
+++ Homework3-MultidimensionalArraysSetsDictionaries/Problem10-PlusRemove/PlusRemove.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PlusRemove
+{
+    public static void Main()
+    {
+        List<char[]> matrix = new List<char[]>();
+        List<List<char>> copyMatrix = new List<List<char>>();
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "END")
+            {
+                break;
+            }
+            matrix.Add(input.ToLower().ToCharArray());
+            copyMatrix.Add(input.ToList());
+        }
+
+        for (int row = 1; row < matrix.Count - 1; row++)
+        {
+            for (int col = 1; col < matrix[row].Length - 1; col++)
+            {
+
+                if (col <= matrix[row - 1].Length - 1 &&
+                    col <= matrix[row + 1].Length - 1)
+                {
+                    if (matrix[row][col] == matrix[row + 1][col] &&
+                        matrix[row][col] == matrix[row - 1][col] &&
+                        matrix[row][col] == matrix[row][col + 1] &&
+                        matrix[row][col] == matrix[row][col - 1])
+                    {
+                        copyMatrix[row][col] = ' ';
+                        copyMatrix[row - 1][col] = ' ';
+                        copyMatrix[row + 1][col] = ' ';
+                        copyMatrix[row][col + 1] = ' ';
+                        copyMatrix[row][col - 1] = ' ';
+                    }
+                }
+            }
+        }
+
+        for (int row = 0; row < matrix.Count; row++)
+        {
+            for (int col = 0; col < copyMatrix[row].Count; col++)
+            {
+                Console.Write(copyMatrix[row][col].ToString().Trim());
+                
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
