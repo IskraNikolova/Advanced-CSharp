44e8eb7c7edfe371825fcd19704e4309db017895
IskraNikolova <air_iskra@yahoo.com>
2015-09-05 23:09:31 +0300

    add .cs

---
+++ Homework3-MultidimensionalArraysSetsDictionaries/Problem7-Phonebook/Phonebook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Phonebook
+{
+    public static void Main()
+    {
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input != "search")
+            {
+              
+                if (phonebook.ContainsKey(input.Split('-')[0]))
+                {
+                    phonebook[input.Split('-')[0]] += "; " + input.Split('-')[1];
+                }
+                else
+                {
+                    phonebook.Add(input.Split('-')[0], input.Split('-')[1]);
+                }
+              
+            }
+            else
+            {
+                break;
+            }
+         
+        }
+        string searchContact = " ";
+        while (searchContact !=  "")
+        {
+            searchContact = Console.ReadLine();
+           
+            if (phonebook.ContainsKey(searchContact))
+            {
+                Console.Write("{0} -> {1}", searchContact, String.Join(", ", phonebook[searchContact]));
+            }
+            else
+            {
+                Console.WriteLine("Contact {0} does not exist.", searchContact);
+            }
+            
+        }
+    }
+}
+
