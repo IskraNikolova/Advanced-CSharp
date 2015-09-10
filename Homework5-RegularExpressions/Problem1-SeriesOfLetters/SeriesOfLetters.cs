22f2b29346e9a5ec8b2a38a0ea4fe8bd1c42f971
IskraNikolova <air_iskra@yahoo.com>
2015-09-10 11:43:18 +0300

    Add a new problem

---
+++ Homework5-RegularExpressions/Problem1-SeriesOfLetters/SeriesOfLetters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class SeriesOfLetters
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        string pattern = @"([a-zA-Z])\1{0,}";
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(input);
+
+        foreach (Match match in matches)
+        {
+            Console.Write(match.Groups[1]);
+        }
+        Console.WriteLine();
+    }
+}
+
