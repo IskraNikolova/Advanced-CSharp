29eca8bda59e560436d43c18de69b52c1771af73
IskraNikolova <air_iskra@yahoo.com>
2015-09-10 21:07:22 +0300

    Add a new problem

---
+++ Homework5-RegularExpressions/Problem4-SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class SentenceExtractor
+{
+    /// <summary>
+    /// A program that reads a keyword and some text from the console and prints all sentences from the text,
+    ///  containing that word. A sentence is any sequence of words ending with '.', '!' or '?'.
+    /// </summary>
+    public static void Main()
+    {
+        string word = Console.ReadLine();
+        string text = Console.ReadLine();
+
+        string pattern = @"([A-Z][\w\W]*?\b"+ word + @"\b[\w\W]*?[.!?])";
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(text);
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match);
+        }
+    }
+}
+
