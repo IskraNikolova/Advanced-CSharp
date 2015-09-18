20b77aa1c6b2dd538bde83e3618638f59ef5430c
IskraNikolova <air_iskra@yahoo.com>
2015-09-18 20:54:02 +0300

    add a new commit

--- Homework3-MultidimensionalArraysSetsDictionaries/Problem3-MatrixShuffling/MatrixShuffing.cs
+++ Homework3-MultidimensionalArraysSetsDictionaries/Problem3-MatrixShuffling/MatrixShuffing.cs
@@ -2,69 +2,72 @@
 
 public class MatrixShuffing
 {
-    public static void Main()
+    private static void Main()
     {
-        int rows = int.Parse(Console.ReadLine());
-        int columns = int.Parse(Console.ReadLine());
+        int row = int.Parse(Console.ReadLine());
+        int column = int.Parse(Console.ReadLine());
 
         //initialize the matrix
-        string[,] matrix = new string[rows, columns];
-        for (int row = 0; row < rows; row++)
+        string[,] matrix = new string[row, column];
+        for (int rows = 0; rows < row; rows++)
         {
-            for (int column = 0; column < columns; column++)
+            for (int col = 0; col < column; col++)
             {
-                matrix[row, column] = Console.ReadLine();
+                matrix[rows, col] = Console.ReadLine();
             }
         }
 
-        bool isEnd = false;
-        while (!isEnd)
+        while (true)
         {
-            string command = Console.ReadLine();
-            if (command == "END")
+            string[] command = Console.ReadLine().Split(' ');
+
+            if (command[0] == "END")
             {
-                isEnd = true;
+                break;
             }
-
-            ////take indices of change of the matrix
-            string[] commands = command.Split(' ');
-
-            if (commands[0] != "swap")
+            if (command[0] == "swap")
             {
-                Console.WriteLine("Invalid input!");
-                continue;
-            }
+                //take indices of change of the matrix
+                int firstRow = int.Parse(command[1]);
+                int firstCol = int.Parse(command[2]);
+                int secondRow = int.Parse(command[3]);
+                int secondCol = int.Parse(command[4]);
 
-            int firstRow = int.Parse(commands[1]);
-            int firstColumn = int.Parse(commands[2]);
-            int secondRow = int.Parse(commands[3]);
-            int secondColumn = int.Parse(commands[4]);
 
-            //checking whether the indices are within the matrix
-            if (firstRow > matrix.GetLength(0) || secondRow > matrix.GetLength(0)
-                || firstColumn > matrix.GetLength(1) || secondColumn > matrix.GetLength(1))
-            {
-                Console.WriteLine("Invalid input!");
-                continue;
-            }
-
-            //replace numbers
-            string middle = matrix[firstRow, firstColumn];
-            matrix[firstRow, firstColumn] = matrix[secondRow, secondColumn];
-            matrix[secondRow, secondColumn] = middle;
-
-            //Print matrix
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                //checking whether the indices are within the matrix
+                if (firstRow < matrix.GetLength(0) ||
+                    firstCol < matrix.GetLength(1) ||
+                    secondRow < matrix.GetLength(0) ||
+                    secondCol < matrix.GetLength(1))
                 {
-                    Console.Write(matrix[row, col] + " ");
+
+                    //replace numbers
+                    string middle = matrix[firstRow, firstCol];
+                    matrix[firstRow, firstCol] = matrix[secondRow, secondCol];
+                    matrix[secondRow, secondCol] = middle;
+
+                    //Print matrix
+                    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            Console.Write("{0} ", matrix[rows, col]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
                 }
 
-                Console.WriteLine();
             }
-        }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
 
+        }
     }
 }
 
