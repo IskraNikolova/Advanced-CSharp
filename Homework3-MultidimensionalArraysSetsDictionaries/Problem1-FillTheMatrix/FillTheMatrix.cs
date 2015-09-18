8b5649062d616dd5b1cae940d4555309dcac90ad
IskraNikolova <air_iskra@yahoo.com>
2015-09-18 16:14:58 +0300

    add a new commit

--- Homework3-MultidimensionalArraysSetsDictionaries/Problem1-FillTheMatrix/FillTheMatrix.cs
+++ Homework3-MultidimensionalArraysSetsDictionaries/Problem1-FillTheMatrix/FillTheMatrix.cs
@@ -9,45 +9,49 @@ public class FillTheMatrix
         int[,] matrix = new int[nSize, nSize];
 
         //Filling a matrix in the regular pattern (top to bottom).
+
+
         for (int row = 0; row < nSize; row++)
         {
             for (int col = 0, num = 0; col < nSize; num = num + nSize, col++)
             {
                 matrix[row, col] = (row + 1) + num;
                 Console.Write("{0} ", matrix[row, col]);
-            }
-
+           }
             Console.WriteLine();
         }
         Console.WriteLine("*****************************");
-    //Filling a matrix in the regular pattern (left to right).
-    int count = 1;
-            for (int row = 0; row < nSize; row++)
-            {
-                if (row%2 == 0)
-                {
-                    for (int column = 0; column < nSize; column++)
-                    {
-                        matrix[column, row] = count++;
-                    }
-                }
-                else
-                {
-                    for (int column = nSize - 1; column >= 0; column--)
-                    {
-                        matrix[column, row] = count++;
-                    }
-                }
-            }
 
-            for (int row = 0; row < nSize; row++)
+
+        //Filling a matrix in the regular pattern (left to right).
+
+        int count = 1;
+        for (int row = 0; row < nSize; row++)
+        {
+            if (row%2 == 0)
             {
-                for (int cow = 0; cow < nSize; cow++)
+                for (int column = 0; column < nSize; column++)
                 {
-                    Console.Write(matrix[row, cow] + " ");
+                    matrix[column, row] = count++;
                 }
-                Console.WriteLine();
             }
+            else
+            {
+                for (int column = nSize - 1; column >= 0; column--)
+                {
+                    matrix[column, row] = count++;
+                }
+            }
+        }
+
+        for (int row = 0; row < nSize; row++)
+        {
+            for (int cow = 0; cow < nSize; cow++)
+            {
+                Console.Write(matrix[row, cow] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
 
