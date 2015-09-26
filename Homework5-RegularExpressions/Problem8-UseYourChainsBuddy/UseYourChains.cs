6c9cb812a9e51076b3df07a8017b8ebc5b6437c0
IskraNikolova <air_iskra@yahoo.com>
2015-09-26 12:35:36 +0300

    Add a new Problem

---
+++ Homework5-RegularExpressions/Problem8-UseYourChainsBuddy/UseYourChains.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class UseYourChainsBuddy
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+
+        string pattern = @".*?(<p>)(.*?)(<\/p>).*?";
+
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(input);
+        StringBuilder result = new StringBuilder();
+        foreach (Match match in matches)
+        {
+            result.Append(match.Groups[2]);
+        }
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (char.IsUpper(result[i]) ||
+                !char.IsLetter(result[i]) &&
+                !char.IsDigit(result[i]))
+            {
+                result[i] = ' ';
+            }
+        }
+
+
+        string value = ChangeLetters(result);
+        string patternForWhiteSpace = @"(\s{2,})";
+        string replaceWhiteSpce = " ";
+        Regex regexForReplace = new Regex(patternForWhiteSpace);
+        string rezultValue = regexForReplace.Replace(value, replaceWhiteSpce);
+        Console.WriteLine(rezultValue);
+    }
+
+    private static string ChangeLetters(StringBuilder builder)
+    {
+        for (int i = 0; i < builder.Length; i++)
+        {
+            if (builder[i] <= 109 && builder[i] != ' ' && !char.IsDigit(builder[i]))
+            {
+                builder[i] += (char)(13);
+            }
+            else if (builder[i] >= 109 && builder[i] != ' ' && !char.IsDigit(builder[i]))
+            {
+                builder[i] -= (char)(13);
+            }
+        }
+        return builder.ToString();
+    }
+
+}
