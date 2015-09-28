523c0ab3840263735db2d0b9a29a2537e6340499
IskraNikolova <air_iskra@yahoo.com>
2015-09-28 12:45:34 +0300

    Add a new Project

---
+++ Homework6-StreamsAndFiles/Problem1-OddLines/OddLines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+public class OddLines
+{
+    /// <summary>
+    /// A program that reads a text file and prints on the console its odd lines. Line numbers starts from 0. Use StreamReader.
+    /// </summary>
+    public static void Main()
+    {
+        using (var reader = new StreamReader("text.txt"))
+        {
+            int lineNumber = 0;
+
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                if (lineNumber%2 != 0)
+                {
+
+                    Console.WriteLine($"{lineNumber}: {line}");
+                }
+                lineNumber++;
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ Homework6-StreamsAndFiles/Problem2-LineNumbers/LineNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+public class LineNumbers
+{
+    /// <summary>
+    /// A program that reads a text file and inserts line numbers in front of each of its lines. 
+    /// The result should be written to another text file. Use StreamReader in combination with StreamWriter.
+    /// </summary>
+    public static void Main()
+    {
+        StreamReader reader = new StreamReader("text.txt");
+        StreamWriter writer = new StreamWriter("copy.txt");
+
+        using (reader)
+        {
+            using (writer)
+            {
+                int lineNumber = 1;
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    writer.WriteLine($"{lineNumber}. {line}");
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
+
