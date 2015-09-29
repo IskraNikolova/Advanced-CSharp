8bfb3397b33f04183ab3f3d70123bbcd29937110
IskraNikolova <air_iskra@yahoo.com>
2015-09-29 10:05:23 +0300

    new commit

--- Homework4-StringsAndTextProcessing/Problem5-UnicodeCharacters/UnicodeCharacters.cs
+++ Homework4-StringsAndTextProcessing/Problem5-UnicodeCharacters/UnicodeCharacters.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 public class UnicodeCharacters
 {
@@ -8,11 +9,32 @@ public class UnicodeCharacters
     public static void Main()
     {
         string input = Console.ReadLine();
-        foreach (char character in input)
-        {
-            Console.Write("\\u00{0:X}", (ushort)character);
-        }
-        Console.WriteLine();
+        PrintCPBytes(input, 8);
+        //foreach (char character in input)
+        //{
+        //    Console.Write("\\u00{0:X}", (ushort)character);
+        //}
+        //Console.WriteLine();
+
+
+    }
+    public static void PrintCPBytes(string str, int codePage)
+    {
+        Encoding targetEncoding;
+        byte[] encodedChars;
+
+        // Gets the encoding for the specified code page.
+        targetEncoding = Encoding.GetEncoding(codePage);
+
+        // Gets the byte representation of the specified string.
+        encodedChars = targetEncoding.GetBytes(str);
+
+        // Prints the bytes.
+        Console.WriteLine
+                 ("Byte representation of '{0}' in Code Page  '{1}':", str,
+                    codePage);
+        for (int i = 0; i < encodedChars.Length; i++)
+            Console.WriteLine("Byte {0}: {1}", i, encodedChars[i]);
     }
 }
 
