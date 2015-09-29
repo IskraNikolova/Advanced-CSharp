16ff9723b91270988391bb05f7d089874945b95d
IskraNikolova <air_iskra@yahoo.com>
2015-09-29 12:59:30 +0300

    Add a new problem

---
+++ Homework6-StreamsAndFiles/Problem9-Disk/Disk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Disk
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int radius = int.Parse(Console.ReadLine());
+        int center = n/2;
+
+        for (int row = 0; row < n; row++)
+        {
+            for (int column = 0; column < n; column++)
+            {
+
+                double checRadius = Math.Sqrt(Math.Pow(column - center, 2) + Math.Pow(row - center, 2));
+                if (checRadius <= radius)
+                {
+                    Console.Write('*');
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
