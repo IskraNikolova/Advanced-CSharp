0163b058e2edb7d4ab69eb1e7f3f8e9edcbc0a5e
IskraNikolova <air_iskra@yahoo.com>
2015-10-05 09:52:38 +0300

    Add a new extension method

---
+++ Homework7-FunctionalProgramming/Problem1-ClassStudent/ExtractStudent.cs
@@ -0,0 +1,15 @@
+﻿namespace Problem1_ClassStudent
+{
+    public static class ExtractStudent
+    {
+        public static bool IsEnrolled(this Student student)
+        {
+            string year = "14";
+            if (student.FacultyNumber.Substring(4, 2) == year)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
