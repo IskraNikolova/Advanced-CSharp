14c27cb5b5787b77a8300d4d9369e7a10ac552f8
IskraNikolova <air_iskra@yahoo.com>
2015-10-05 09:51:56 +0300

    Add a new extension method

---
+++ Homework7-FunctionalProgramming/Problem1-ClassStudent/ExtensionMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem1_ClassStudent
+{
+    public static class ExtensionMethod
+    {
+        public static bool IsHasTwoMarks(this Student student)
+        {
+            if (student.Marks.Count == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
