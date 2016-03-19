b4605edacdad01fe11de3511a6989ea339edf174
IskraNikolova <air_iskra@yahoo.com>
2016-03-19 11:49:31 +0200

    Add helper for bit operations

---
+++ BitArray/BitArray32/BitArray32.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BitArray32
+{
+    public class BitArray32
+    {
+        private int value;
+
+        public BitArray32(int value)
+        {
+            this.value = value;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > 31)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(index), "Index must be in the range[0...31]");
+                }
+
+                int bit = (this.value >> index) & 1;
+
+                return bit;
+            }
+            set
+            {
+                if (index < 0 || index > 31)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(index), "Index must be in the range[0...31]");
+                }
+
+                if (value != 0 && value != 1)
+                {
+                    throw new ArgumentException("Argument must be either 1 or 0");
+                }
+
+                int mask = 1;
+                this.value &= (mask << index);
+                this.value |= value << index;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+    }
+}
---
+++ BitArray/BitArray32/BitArrayTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BitArray32
+{
+    public class BitArrayTest
+    {
+        public static void Main()
+        {
+            BitArray32 seven = new BitArray32(7);
+            seven[7] = 1;           
+            Console.WriteLine(seven);            
+        }
+    }
+}
